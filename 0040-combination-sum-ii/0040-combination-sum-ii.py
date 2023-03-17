e09ab676d4ec1211cae456204466509de7ac923b
aso2001 <74437020+aso2001@users.noreply.github.com>
2023-03-16 23:04:27 -0400

    Time: 63 ms (76.86%), Space: 14 MB (51.81%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        # Recursive solution using list sorting of candidates
+        res2 = []
+
+        def dfs(candidates, rem, res):
+            if rem == 0:
+                res2.append(res.copy())
+                return
+
+            for i in range(len(candidates)):
+                if i > 0 and candidates[i] == candidates[i - 1]:
+                    continue
+                if rem >= candidates[i]:
+                    res.append(candidates[i])
+                    dfs(candidates[i + 1:], rem - candidates[i], res)
+                    res.pop()
+
+        candidates.sort()
+        dfs(candidates, target, [])
+        return res2
