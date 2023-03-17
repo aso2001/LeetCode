4c7a05939e2dea944c5b952a7e741b8f6d45a5f1
aso2001 <74437020+aso2001@users.noreply.github.com>
2023-03-16 21:03:55 -0400

    Time: 2570 ms (83.81%), Space: 52.7 MB (75.51%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        # Sieve of Eratosthenes Algorithm O(n*log(log(n))) time complexity
+        if n <= 1: return 0
+        prime = [True]*n
+        prime[0], prime[1] = False, False
+        for i in range(2, int(n**0.5) + 1):
+            if prime[i]:
+                for j in range(i*i, n, i):
+                    prime[j] = False
+        return sum(prime) 
