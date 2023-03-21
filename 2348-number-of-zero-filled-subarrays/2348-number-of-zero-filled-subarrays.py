02528c40b0f682bd961c2f288a240c51ee0106b7
aso2001 <74437020+aso2001@users.noreply.github.com>
2023-03-21 14:21:24 -0400

    Time: 1108 ms (57.95%), Space: 24.7 MB (30.39%) - LeetHub

--- 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.py
@@ -1,5 +1,17 @@
 class Solution:
     def zeroFilledSubarray(self, nums: List[int]) -> int:
+        # O(n) time O(1) space solution 
+        res = cnt = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                cnt += 1
+            else:
+                cnt = 0
+            res += cnt
+        return res
+
+
+    def zeroFilledSubarray2(self, nums: List[int]) -> int:
         
         res = [0]*len(nums)
         flag = 0
