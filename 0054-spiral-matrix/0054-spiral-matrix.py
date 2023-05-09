845525ed92ac6c4b040f1d456cc2216113096cc8
aso2001 <74437020+aso2001@users.noreply.github.com>
2023-05-08 20:43:54 -0400

    Time: 55 ms (5.34%), Space: 16.3 MB (14.84%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,5 +1,37 @@
 class Solution:
     def spiralOrder(self, mx: List[List[int]]) -> List[int]:
+        cnt = 0
+        res = []
+        L, R, U, D = 0, len(mx[0]) - 1, 0, len(mx) - 1
+        while True:
+            for j in range(L, R + 1):
+                res.append(mx[U][j])
+                cnt += 1
+            if cnt == len(mx[0])*len(mx):
+                break
+            for i in range(U + 1, D + 1):
+                res.append(mx[i][R])
+                cnt += 1
+            if cnt == len(mx[0])*len(mx):
+                break
+            for j in range(R - 1, L - 1, -1):
+                res.append(mx[D][j])
+                cnt += 1
+            if cnt == len(mx[0])*len(mx):
+                break
+            for i in range(D - 1, U, -1):
+                res.append(mx[i][L])
+                cnt += 1
+            if cnt == len(mx[0])*len(mx):
+                break
+            U += 1
+            D -= 1
+            L += 1
+            R -= 1
+        return res
+    
+    
+    def spiralOrder2(self, mx: List[List[int]]) -> List[int]:
         res, i1, i2, j1, j2 = [], 0, len(mx), 0, len(mx[0])
 
         while True:
