b8a8129f410664fc3ce3d618c84d796f98239915
aso2001 <74437020+aso2001@users.noreply.github.com>
2023-05-26 20:05:41 -0400

    Time: 3945 ms (5.43%), Space: 20.5 MB (39.13%) - LeetHub

--- 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.py
+++ 1775-equal-sum-arrays-with-minimum-number-of-operations/1775-equal-sum-arrays-with-minimum-number-of-operations.py
@@ -1,24 +1,19 @@
 class Solution:
     def minOperations(self, nums1: List[int], nums2: List[int]) -> int:
-        if len(nums2) > len(nums1):
-            nums1, nums2 = nums2, nums1
-        if len(nums1) > len(nums2):
-            if len(nums1) > len(nums2) * 6:
-                return -1
         n1 = len(nums1)
         n2 = len(nums2)
         s1 = sum(nums1)
         s2 = sum(nums2)
-        cnt = 0
         if s1 == s2:
-            return cnt
+            return 0
 
+        cnt = 0
         mx, mn = [], []
         if s2 > s1:
             nums1, nums2 = nums2, nums1
             s1, s2, n1, n2 = s2, s1, n2, n1
-        nums1.sort()
-        nums2.sort(reverse = True)
+        nums1.sort(reverse = True)
+        nums2.sort()
         for i in range(n1):
             if nums1[i] > 1:
                 mx.append(nums1[i] - 1)
@@ -28,14 +23,16 @@ class Solution:
 
         while s1 > s2:
             if mx and mn:
-                if mx[-1] >= mn[-1]:
-                    s1 -= mx.pop()
+                if mx[0] >= mn[0]:
+                    s1 -= mx.pop(0)
                 else:
-                    s2 += mn.pop()
+                    s2 += mn.pop(0)
             elif mx or mn:
                 if mx:
-                    s1 -= mx.pop()
+                    s1 -= mx.pop(0)
                 else:
-                    s2 += mn.pop()
+                    s2 += mn.pop(0)
+            else:
+                return -1
             cnt += 1
         return cnt
