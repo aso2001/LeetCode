c60a3788b0f1ad9d4a44aa8e97dec826be4be6b8
aso2001 <74437020+aso2001@users.noreply.github.com>
2023-06-02 19:51:10 -0400

    Time: 739 ms (77.48%), Space: 16.7 MB (34.46%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,39 +1,29 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         adj = defaultdict(list)
-        idx = list(range(len(bombs)))
-        comb = combinations(idx, 2)
-        for c in comb:
-            a, b = c
-            x1, y1, r1 = bombs[a]
-            x2, y2, r2 = bombs[b]
-            if (x1 - x2)**2 + (y1 - y2)**2 <= r1*r1:
-                adj[a].append(b)
-            if (x1 - x2)**2 + (y1 - y2)**2 <= r2*r2:
-                adj[b].append(a)
-        
-        def bfs(s):
+        for i in range(len(bombs)-1):
+            for j in range(i+1, len(bombs)):
+                x1, y1, r1 = bombs[i]
+                x2, y2, r2 = bombs[j]
+                if (x1 - x2)**2 + (y1 - y2)**2 <= r1*r1:
+                    adj[i].append(j)
+                if (x1 - x2)**2 + (y1 - y2)**2 <= r2*r2:
+                    adj[j].append(i)
+            
+        def dfs(s):
             q = deque()
-            nxt = deque()
             q.append(s)
             visited = [0]*len(bombs)
             visited[s] = 1
-            cnt = 1
-            while True:
-                while q:
-                    cur = q.popleft()
-                    for nei in adj[cur]:
-                        if not visited[nei]:
-                            visited[nei] = 1
-                            nxt.append(nei)
-                            cnt += 1
-                if nxt:
-                    q = nxt.copy()
-                    nxt = deque()
-                else:
-                    return cnt
+            while q:
+                cur = q.popleft()
+                for nei in adj[cur]:
+                    if not visited[nei]:
+                        visited[nei] = 1
+                        q.append(nei)
+            return sum(visited)
                 
         res = 0
-        for b in idx:
-            res = max(res, bfs(b))
-        return res        
+        for b in range(len(bombs)):
+            res = max(res, dfs(b))
+        return res      
