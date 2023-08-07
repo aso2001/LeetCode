0e2a10506fd0701b7e56092e3d042c6dd1b8bf1b
aso2001 <74437020+aso2001@users.noreply.github.com>
2023-08-07 10:39:15 -0400

    Time: 45 ms (96.50%), Space: 16.9 MB (64.57%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,5 +1,5 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+    def searchMatrix2(self, matrix: List[List[int]], target: int) -> bool:
         # Binary search first ROW then COL. Time compexity O(log(m*n))
         # search row
         l, r = 0, len(matrix) - 1
@@ -20,6 +20,34 @@ class Solution:
                 r = mid - 1
             elif target > matrix[row][mid]:
                 l = mid + 1
+            else:
+                return True
+        return False
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        mx = matrix
+        row = 0
+        
+        L, R = 0, len(mx) - 1
+        while L <= R:
+            mid  = (L + R) // 2
+            if mx[mid][0] > target:
+                R = mid - 1
+            elif mx[mid][len(mx[0]) - 1] < target:
+                L = mid + 1
+            elif mx[mid][0] < target < mx[mid][len(mx[0]) - 1]:
+                row = mid
+                break
+            elif mx[mid][0] == target or target == mx[mid][len(mx[0]) - 1]:
+                return True
+
+        L, R = 0, len(mx[0]) - 1
+        while L <= R:
+            mid = (L + R) // 2
+            if mx[row][mid] > target:
+                R = mid - 1
+            elif mx[row][mid] < target:
+                L = mid + 1
             else:
                 return True
         return False
