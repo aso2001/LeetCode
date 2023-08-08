5ac9804fc892ce58f66e1b88cfcfdea7c3b08f92
aso2001 <74437020+aso2001@users.noreply.github.com>
2023-08-08 19:22:38 -0400

    Time: 570 ms (34.46%), Space: 18.7 MB (28.38%) - LeetHub

--- 2013-detect-squares/2013-detect-squares.py
+++ 2013-detect-squares/2013-detect-squares.py
@@ -1,48 +1,19 @@
 class DetectSquares:
-
     def __init__(self):
-        self.dp = {}
+        self.dp = defaultdict(int)
+        self.pt = []
 
     def add(self, point: List[int]) -> None:
         pp = tuple(point)
-        if pp not in self.dp:
-            self.dp[pp] = 1
-        else:
-            self.dp[pp] += 1
+        self.dp[pp] += 1
+        self.pt.append(point)
 
     def count(self, point: List[int]) -> int:
-        x, y = set(), set()
-        for d in self.dp.keys():
-            if d[0] == point[0]:
-                y.add(d[1])
-            elif d[1] == point[1]:
-                x.add(d[0])
         res = 0
-        for xx in x:
-            x1 = xx
-            y1 = point[1] - abs(point[0] - xx)
-            pp = (x1, y1)
-            tmp = 1
-            if y1 in y and pp in self.dp:
-                tmp *= self.dp[pp]
-                tt1 = (point[0], y1)
-                tt2 = (x1, point[1])
-                tmp *= self.dp[tt1]
-                tmp *= self.dp[tt2]
-                res += tmp
-
-            y1 = point[1] + abs(point[0] - xx)
-            pp = (x1, y1)
-            tmp = 1
-            if y1 in y and pp in self.dp:
-                tmp *= self.dp[pp]
-                tt1 = (point[0], y1)
-                tt2 = (x1, point[1])
-                tmp *= self.dp[tt1]
-                tmp *= self.dp[tt2]
-                res += tmp
+        for x, y in self.pt:
+            if abs(point[1] - y) == abs(point[0] - x) and x != point[0] and y != point[1]:
+                res += self.dp[(x, point[1])] * self.dp[(point[0], y)]
         return res
-
 
 # Your DetectSquares object will be instantiated and called as such:
 # obj = DetectSquares()
