a3a2b0b520d9f74f5449a4ed9cccec99d8edb7cf
aso2001 <74437020+aso2001@users.noreply.github.com>
2023-08-24 11:54:37 -0400

    Time: 41 ms (68.15%), Space: 16.4 MB (69.23%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -4,44 +4,40 @@ class Solution:
         q = deque()
         for w in words: q.append(w)
 
-        tmp, lines, size = [], [], deque()
-        tmp_size = 0
+        line, lines, size, line_size = [], [], deque(), 0
         while q:
-            wrd = q[0]
-            if tmp_size + len(wrd) + len(tmp) <= maxWidth:
-                tmp_size += len(wrd)
-                tmp.append(q.popleft())
+            w = q[0]
+            if line_size + len(w) + len(line) <= maxWidth:
+                line_size += len(w)
+                line.append(q.popleft())
             else:
-                lines.append(tmp)
-                size.append(tmp_size)
-                tmp = []
-                tmp_size = 0
-        lines.append(tmp)
-        size.append(tmp_size)
+                lines.append(line)
+                size.append(line_size)
+                line, line_size = [], 0
+        lines.append(line)
+        size.append(line_size)
 
         res = []
         for line in lines:
-            newl = ''
-            lsize = size.popleft()
-            spc = maxWidth - lsize
-            if len(line) == 1:
-                newl += line[0]
-                newl += ' '*spc
-                res.append(newl)
-            else:
-                nwrd = len(line)
-                if line == lines[-1]:
+            new_line = ''
+            spc = maxWidth - size.popleft()
+            num_w = len(line)
+            if num_w > 1:
+                if line != lines[-1]:
                     for w in line:
-                        newl += w
-                        newl += ' '*(1 if nwrd > 1 else spc)
-                        spc -= 1
-                        nwrd -= 1
+                        new_line += w
+                        nxt_spc = (0 if num_w == 1 else math.ceil(spc/(num_w - 1)))
+                        new_line += ' '*nxt_spc
+                        spc -= nxt_spc
+                        num_w -= 1
                 else:
                     for w in line:
-                        newl += w
-                        nxt_spc = (0 if nwrd == 1 else math.ceil(spc/(nwrd - 1)))
-                        newl += ' '*nxt_spc
-                        spc -= nxt_spc
-                        nwrd -= 1
-                res.append(newl)
+                        new_line += w
+                        new_line += ' '*(1 if num_w > 1 else spc)
+                        spc -= 1
+                        num_w -= 1
+            else:
+                new_line += line[0]
+                new_line += ' '*spc
+            res.append(new_line)
         return res
