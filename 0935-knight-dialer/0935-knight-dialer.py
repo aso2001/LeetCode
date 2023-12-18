125a07292414cdc0e882e3a77b37e1030f68d26f
aso2001 <74437020+aso2001@users.noreply.github.com>
2023-12-18 05:34:06 -0500

    Time: 98 ms (94.35%), Space: 16.4 MB (84.76%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -0,0 +1,15 @@
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        if n == 1:
+            return 10
+            
+        A = 4
+        B = 2
+        C = 2
+        D = 1
+        mod = 10 ** 9 + 7
+        
+        for _ in range(n - 1):
+            A, B, C, D = (2 * (B + C)) % mod, A, (A + 2 * D) % mod, C 
+        
+        return (A + B + C + D) % mod
