ab7520cad7f6aea6d65cdd7edb7bf6dd1e5a664b
aso2001 <74437020+aso2001@users.noreply.github.com>
2024-02-04 20:08:54 -0500

    Time: 80 ms (81.45%), Space: 16.6 MB (89.99%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -2,6 +2,15 @@ class Solution:
     def firstUniqChar(self, s: str) -> int:
         dd = {}
         for i in range(len(s)):
+            if s[i] in dd:
+                dd[s[i]] = 10**6
+            else:
+                dd[s[i]] = i
+        return min(dd.values()) if min(dd.values()) <= 10**5 else -1
+
+    def firstUniqChar2(self, s: str) -> int:
+        dd = {}
+        for i in range(len(s)):
             idx = ord(s[i]) - ord('a')
             if idx in dd:
                 dd[idx] = len(s)
