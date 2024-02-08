3c25932658f5914aa4ff4362c2c4efb802c596b3
aso2001 <74437020+aso2001@users.noreply.github.com>
2024-02-08 11:41:40 -0500

    Time: 4673 ms (11.22%), Space: 23.9 MB (19.04%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.py
+++ 0279-perfect-squares/0279-perfect-squares.py
@@ -1,6 +1,29 @@
 class Solution:
     def numSquares(self, n: int) -> int:
         dp = {}
+        sqrs = []
+        mnsqr = int(sqrt(n))
+        if mnsqr == sqrt(n): return 1
+        for i in range(1, mnsqr + 1):
+            sqrs.append(i*i)
+        sq = set(sqrs)
+
+        def dfs(i):
+            if i in sq:
+                dp[i] = 1
+                return 1
+            if i in dp:
+                return dp[i]
+            dp[i] = math.inf
+            for j in sqrs:
+                if i - j < 0:
+                    break
+                dp[i] = min(1 + dfs(i - j), dp[i])
+            return dp[i]
+        return dfs(n)
+
+    def numSquares2(self, n: int) -> int:
+        dp = {}
         sq = []
         if int(sqrt(n)) == sqrt(n): return 1 
         mxn = int(sqrt(n))
