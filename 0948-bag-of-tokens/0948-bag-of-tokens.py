cf21bc064745ab10d8b7b86443376b1d20798a39
aso2001 <74437020+aso2001@users.noreply.github.com>
2024-03-03 23:01:47 -0500

    Time: 57 ms (34.15%), Space: 16.8 MB (64.63%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.py
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -1,19 +1,17 @@
 class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
         tokens.sort()
-        if sum(tokens) <= power: return len(tokens) 
- 
+        
         res, L, R = 0, 0, len(tokens) - 1
         while L <= R:
             if tokens[L] <= power:
                 res += 1
                 power -= tokens[L]
                 L += 1
+            elif L < R and res >= 1:
+                res -= 1
+                power += tokens[R]
+                R -= 1
             else:
-                if L < R and res >= 1:
-                    res -= 1
-                    power += tokens[R]
-                    R -= 1
-                else:
-                    return res
-        return res 
+                return res
+        return res
