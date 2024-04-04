f5e73f026398038a1a27c10c06e4a81fd094aa0a
aso2001 <74437020+aso2001@users.noreply.github.com>
2024-04-03 20:22:10 -0400

    Time: 30 ms (86.71%), Space: 16.6 MB (7.45%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -1,8 +1,7 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-        stack = []
-        i, res = 0, 0
-        while i < len(s):
+        stack, res = [], 0
+        for i in range(len(s)):
             if s[i] != '(' and s[i] != ')':
                 pass
             elif s[i] == '(':
@@ -10,5 +9,4 @@ class Solution:
                 res = max(res, len(stack))
             elif s[i] == ')':
                 stack.pop()
-            i += 1
         return res
