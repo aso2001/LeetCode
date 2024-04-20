917f9b52043422710102f60d3a94329ecb81e2c8
aso2001 <74437020+aso2001@users.noreply.github.com>
2024-04-20 19:56:08 -0400

    Time: 845 ms (59.15%), Space: 30.1 MB (80.28%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+        res = []
+        m, n = len(land), len(land[0])
+        
+        def dfs(i, j):
+            if i == m or j == n or land[i][j] == 0:
+                return [0,0]
+            land[i][j] = 0
+            a1 = dfs(i, j+1)
+            a2 = dfs(i+1, j)
+            return [max(i, a1[0], a2[0]), max(j, a1[1], a2[1])]
+
+        for i in range(len(land)):
+            j = 0
+            while j < n:
+                if land[i][j]:
+                    arr = dfs(i, j)
+                    res.append([i,j] + arr)
+                    j = arr[1]
+                j += 1
+        return res
