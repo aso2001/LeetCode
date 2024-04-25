8e133d31545bc6b7c92c7045e48afb83c206380c
aso2001 <74437020+aso2001@users.noreply.github.com>
2024-04-25 19:51:58 -0400

    Time: 1587 ms (34.31%), Space: 17.4 MB (68.61%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -2,9 +2,10 @@ class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
         dp = [0]*26
         for i in range(len(s)):
-            tmp = 0
+            cur = ord(s[i]) - ord('a')
+            longest = 0
             for prev in range(26):
-                if abs(ord(s[i]) - ord('a') - prev) <= k:
-                    tmp = max(tmp, dp[prev])
-            dp[ord(s[i]) - ord('a')] = max(dp[ord(s[i]) - ord('a')], tmp + 1)
+                if abs(cur - prev) <= k:
+                    longest = max(longest, 1 + dp[prev])
+            dp[cur] = max(dp[cur], longest)
         return max(dp)
